670d8518ea3c2266381552cecefa21b1630866ad
Deepak Maurya <deepshal99@gmail.com>
2021-07-08 20:51:11 +0530

    Co-authored-by: rabbitlover260 <rabbitlover260@users.noreply.github.com> Co-authored-by: Guruprasad Dalvi <GuruprasadDalvi@users.noreply.github.com>

--- oneprofile/src/App.js
+++ oneprofile/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import {Heading, VStack} from '@chakra-ui/react';
+import { Heading, VStack } from '@chakra-ui/react';
 import Signup from './components/Signup'
 import Login from './components/Login'
 import Home from './components/Home'
-import RegisterContainer from './components/RegisterContainer';
+import { Link, } from 'react-router-dom'
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
     return (
